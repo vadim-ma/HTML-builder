327fbf35bc1cf3b39c0bc5d26fadd2d8fbb12b1d
vadim-ma <color.ui>
2024-01-20 12:02:40 +0300

    feat: finish task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,31 @@
+const path = require('node:path');
+const readline = require('node:readline');
+const fs = require('node:fs/promises');
+
+(async function () {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const outFileName = path.join(__dirname, '02-write-file.txt');
+  const outFh = await fs.open(outFileName, 'w');
+  const outStream = outFh.createWriteStream({
+    encoding: 'utf8',
+    autoClose: true,
+  });
+  process.on('exit', () => {
+    outFh.close();
+  });
+  console.log("Enter text. Use 'exit' to finish"); // eslint-disable-line quotes
+  rl.setPrompt('> ');
+  rl.prompt();
+  rl.on('line', (input) => {
+    if (input === 'exit') {
+      rl.close();
+      process.exit();
+    }
+    outStream.write(input + '\n');
+    rl.prompt();
+  });
+})();
