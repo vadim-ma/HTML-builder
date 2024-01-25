85dd4d833f220750c0e3551b5c6970a766ffc792
vadim-ma <color.ui>
2024-01-25 19:04:15 +0300

    fix: fix an error after deleting dst folder in the 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,16 +1,13 @@
-const path = require('node:path');
-const fs = require('node:fs/promises');
-
-async function prepDst(dst) {
-  await fs.rm(dst, { recursive: true, force: true });
-  await fs.mkdir(dst, { recursive: true });
-}
+const path = require("node:path");
+const fs = require("node:fs/promises");
 
 async function copyFile(name, src, dst) {
   await fs.copyFile(path.join(src, name), path.join(dst, name));
 }
 
 async function copyDir(src, dst) {
+  await fs.mkdir(dst, { recursive: true });
+
   const dir = await fs.readdir(src, { withFileTypes: true });
   for (const entry of dir) {
     if (!entry.isFile()) {
@@ -20,6 +17,11 @@ async function copyDir(src, dst) {
   }
 }
 
-const dst = path.join(__dirname, 'files-copy');
-prepDst(dst);
-copyDir(path.join(__dirname, 'files'), dst);
+async function buildAssets(src, dst) {
+  await fs.rm(dst, { recursive: true, force: true });
+  await copyDir(src, dst);
+}
+
+const src = path.join(__dirname, "files");
+const dst = path.join(__dirname, "files-copy");
+buildAssets(src, dst);
